6637c53b03284c6a207442e16bc15a8b4ca409ab
Kirill Arkhipov <arkhipov.km@gmail.com>
2021-07-29 20:22:01 +0200

    some refactoring

--- Sqlite_to_texte_files.py
+++ Sqlite_to_texte_files.py
@@ -1,16 +1,7 @@
-from datetime import datetime
 import numpy as np
-import pandas as pd
-from munch import Munch, munchify
-import warnings
-warnings.filterwarnings('ignore')
-
-
 
 import sqlite3
 import json
-
-
 
 SPOT_NUM = 64      # number of spots
 PEAKS_PER_SPOT = 3 # 3 peaks per spot
@@ -36,39 +27,34 @@ database = r"C:\Users\RomainDubreuil\Aryballe\hw - General\200420-watch\algorith
 ######## That was the only place to adapt.
 
 record_id = '1622802740113_NOA221-100010-CS220-1145_fsp'
-data_from_sqlite = read_record_in_sqlite(database, record_id)
 
+# data_from_sqlite = read_record_in_sqlite(database, record_id)
+# with open('record.json', 'w') as f:
+#     json.dump(data_from_sqlite, f)
+
+with open('record.json', 'r') as f:
+    data_from_sqlite = json.load(f)
 
 # Write mmi a, b and c
 mmi_a = data_from_sqlite["timeseries"]["osense:mmi.a"]
 mmi_b = data_from_sqlite["timeseries"]["osense:mmi.b"]
 mmi_c = data_from_sqlite["timeseries"]["osense:mmi.c"]
 
-mmi_a_txt = []
-mmi_b_txt = []
-mmi_c_txt = []
-time_mmi = []
 
+mmi_all = []
+for (a_frame, _), (b_frame, _), (c_frame, _) in zip(mmi_a, mmi_b, mmi_c):
+    mmi_line = []
+    for a, b, c in zip(a_frame, b_frame, c_frame):
+        mmi_line.extend([a, b, c])  # Extrnd is like append(a), apend(b), append(c), but in one line
+    assert len(mmi_line) == 192
+    mmi_all.append(mmi_line)  # Append a line with 192 values
 
-for x in mmi_a:
-    mmi_a_txt.append(x[0])
-    time_mmi.append(x[1]) 
-for x in mmi_b:
-    mmi_b_txt.append(x[0])
-    
-for x in mmi_c:
-    mmi_c_txt.append(x[0])
-   
-
-np.savetxt('MMI_a.txt',mmi_a_txt,fmt='%.8f')
-np.savetxt('MMI_b.txt',mmi_b_txt,fmt='%.8f')
-np.savetxt('MMI_c.txt',mmi_c_txt,fmt='%.8f')
-
-np.savetxt('time_mmi.txt',time_mmi,fmt='%.8f')
+np.savetxt('mmi_all.txt',mmi_all,fmt='%.18f')
 
 # Write sqlite sensorgram
 data_mzi = data_from_sqlite["data"]
-np.savetxt('data_from_sqlite.txt',data_mzi,fmt='%.8f')
+data_mzi = np.reshape(data_mzi, [-1, 64])
+np.savetxt('mzi_all.txt',data_mzi,fmt='%.18f')
 
 
 
